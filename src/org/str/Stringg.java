1e1aed8b8341be194b00cfbbbbcf57c35ab74a2b
oviyatamil <oviyanidhiece@gmail.com>
2023-06-17 19:25:28 +0530

    initial commit

---
+++ src/org/str/Stringg.java
@@ -0,0 +1,172 @@
+package org.str;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Stringg {
+public static void main(String[] args) {
+//	System.out.println("------REPLACE ALL-----");
+//	String s = "Welcome";
+//	String replaceAll = s.replaceAll("Welcome", "W@lc@m@");
+//	System.out.println(replaceAll);
+//	System.out.println();
+//	
+//	System.out.println("------SUBSTRING-----");
+//	String s1 = "Welcome to java class";
+//	String substring = s1.substring(0, 7);
+//	System.out.println(substring);
+//	System.out.println();
+//	
+//	System.out.println("----SPLIT----");
+//	String[] sp = s1.split(" ");
+//	for(String x : sp) {
+//	System.out.println(x);
+//	}
+//	System.out.println();
+//	
+//	String[] k = s1.split("l");
+//	for(String y : k) {
+//	System.out.println(y);
+//	System.out.println();
+//	}
+//	
+//	System.out.println("-------EQUALS------");
+//	String s4 = "Nisha";
+//	String s5 = "nisha";
+//	
+//	boolean equals = s4.equals(s5);
+//	System.out.println(equals);
+//	System.out.println();
+//	
+//	System.out.println("          --------ISEMPTY------");
+//	String s6 = "";
+//	boolean empty = s6.isEmpty();
+//	System.out.println(empty);
+//	System.out.println();
+//	
+//	System.out.println("           -------ENDSWITH------");
+//	boolean endsWith = s1.endsWith("java");
+//	System.out.println(endsWith);
+//	
+//	System.out.println();
+//	
+//	System.out.println("          -------STARTS WITH-----");
+//	boolean startsWith = s1.startsWith("hai");
+//	System.out.println(startsWith);
+//	
+//	System.out.println("        -------UPPER/LOWERCASE-----");
+//	String h = "Hai Sri";
+//	String upperCase = h.toUpperCase();
+//	System.out.println( upperCase);
+//	System.out.println();
+//	String lowerCase = h.toLowerCase();
+//	System.out.println(lowerCase);
+//	System.out.println();
+//
+//	System.out.println("         --------REPLACE-------");
+//	String u = "oviyanidhi27@gmail.com";
+//	boolean contains = u.contains("gmail");
+//	//System.out.println(contains);
+//	if(contains == true) {
+//	String replace = u.replace("gmail", "yahoo");
+//	System.out.println(replace);
+//	System.out.println();
+//	}
+//	
+//	String n = s1.replace(" ", "#");
+//	System.out.println(n);
+//	
+//	
+//	System.out.println("             ------LENGTH------");
+//	String ph = "12345678901";
+//	int length = ph.length();
+//	System.out.println(length);
+//	if(length > 10) {
+//		System.out.println("invalid");
+//		System.out.println();
+//	}
+//	System.out.println("valid");
+//	
+//	
+//	System.out.println(         "------EQUALS IGNORE CASE--------");
+//	String st = "java";
+//	String sf = "JAVA";
+//	System.out.println(st.equalsIgnoreCase(sf));
+//	
+//	System.out.println("              -------CHAR AT---------");
+//	String kl = "j a v a p r o g r a m ";
+//	char charAt = kl.charAt(8);
+//	System.out.println(charAt);
+//	
+//	System.out.println("               ----INDEXOF----");
+//	int lastIndexOf = kl.lastIndexOf("");
+//	System.out.println(lastIndexOf);
+//	System.out.println();
+//	
+//	int indexOf = kl.indexOf("a");
+//	System.out.println(indexOf);
+	
+//	string swap using 3rd variable
+//	String n1 = "nila";
+//	String n2 = "oviya";
+//	String n3;
+//	n3=n1;
+//	n1=n2;
+//	n2=n3;
+//	System.out.println(n1);
+//	System.out.println(n2);
+	
+	//swap without 3rd variable
+//	n1 = n1+n2;
+//	
+//	n2 = n1.substring(0,n1.length()-n2.length());
+//	n1=n1.substring(n2.length());
+//
+//	
+//	System.out.println(n1);
+//	System.out.println(n2);
+	
+	
+	//covert every first letter to CAPS
+//	String n1 = "laugh and love";
+//	String s=" ";
+//	String[] split = n1.split(" ");
+//	for(int i = 0;i<split.length;i++) {
+//		String c = split[i].substring(0, 1);
+//		String up = c.toUpperCase();
+//		s=s+up+split[i].substring(1)+" ";
+//		
+//	}
+//	System.out.println(s);
+	
+	//STRING REVERSE USING REVERSE
+	
+	String s = "nilan";
+//	StringBuffer s1 = new StringBuffer("oviya");
+//	StringBuffer reverse = s1.reverse();
+//	System.out.println(reverse);
+//	
+//	//STRING REVERSE USING LOOP
+//	for (int i = s.length()-1; i >= 0 ; i--) {
+//		char c = s.charAt(i);
+//		System.out.print(c);
+//	}
+	//PALINDROME
+	
+	
+	
+	
+     //print count of each letters
+	
+//		String k = "oviya and sri";
+//		String[] split = k.split("");
+//		List<String> l = new ArrayList<>();
+//		l.add(split);
+//		for(String x :split) {
+//			System.out.println(x);
+//		}
+
+	}
+}
+
+
